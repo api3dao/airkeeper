282fed581e2d3b93e3ad4e9846599a6450fd50e2
vponline <vepant@gmail.com>
2022-03-22 17:43:53 +0200

    Fix check-conditions test with retries enabled

--- src/evm/check-conditions.test.ts
+++ src/evm/check-conditions.test.ts
@@ -159,7 +159,7 @@ describe('checkSubscriptionCondition', () => {
 
     expect(getFunctionSpy).toHaveBeenCalledTimes(1);
     expect(conditionPspBeaconUpdateSpy).not.toHaveBeenCalled();
-    expect(conditionPspBeaconUpdateErrorSpy).toHaveBeenCalledTimes(1);
+    expect(conditionPspBeaconUpdateErrorSpy).toHaveBeenCalledTimes(2);
     expect(logs).toEqual([
       {
         error: unexpectedError,
