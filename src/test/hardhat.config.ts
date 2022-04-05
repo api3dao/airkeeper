af431cb6a481593e169dcabc791d191dcce98787
vponline <vepant@gmail.com>
2022-04-05 14:36:29 +0300

    Fix hardhat config

--- src/test/hardhat.config.ts
+++ src/test/hardhat.config.ts
@@ -1,16 +1,11 @@
 import { HardhatUserConfig } from 'hardhat/types';
 
 const config: HardhatUserConfig = {
-  defaultNetwork: 'localhost',
   networks: {
-    hardhat: {
-      chainId: 1337,
-    },
     localhost: {
       url: 'http://127.0.0.1:8545/',
     },
   },
-  solidity: '0.8.9',
 };
 
 export default config;
